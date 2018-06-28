2a3ee5aa786522a8d6b6da82d3885194821613b6
= <=>
2018-06-27 22:25:04 -0400

    continuação a partir de 1h de vídeo até o final... finalizando o workshop

--- admin.js
+++ admin.js
@@ -15,23 +15,71 @@ const init = connection => {
         res.send('olá admin')
     })
 
-    app.get('/games', async(req, res) => {
-        const [rows, fields ] = await connection.execute('select * from games')
+    app.get('/games', async (req, res) => {
+        const [rows, fields] = await connection.execute('select * from games')
         res.render('admin/games', {
             games: rows
         })
     })
 
-    app.post('/games', async(req, res) => {
+    app.post('/games', async (req, res) => {
         const { team_a, team_b } = req.body
         await connection.execute('insert into games (team_a, team_b) values (?,?)', [
             team_a, team_b
         ])
 
         res.redirect('/admin/games')
-    } )
+    })
 
-    app.get('/games/delete/:id', async(req, res) => {
+
+    app.post('/games/results', async (req, res) => {
+        const games = []
+        Object
+            .keys(req.body)
+            .forEach(team => {
+                const parts = team.split('_')
+                const game = {
+                    game_id: parseInt(parts[1]),
+                    result_a: parseInt(req.body[team].a),
+                    result_b: parseInt(req.body[team].b)
+                }
+                games.push(game)
+            })
+        for (let i = 0; i < games.length; i++) {
+            const game = games[i]
+            const [guessings] = await connection.execute('select * from guessings where game_id = ? ', [
+                game.game_id
+            ])
+
+            const batch = guessings.map(guess => {
+                let score = 0
+                if (guess.result_a === game.result_a && guess.result_b === game.result_b) {
+                    score = 100
+                } else {
+                    if (guess.result_a === game.result_a || guess.result_b === game.result_b) {
+                        score += 25
+
+                        if (guess.result_a < guess.result_b && game.result_a < game.result_b) {
+                            score += 25
+                        }
+                        if (guess.result_a > guess.result_b && game.result_a > game.result_b) {
+                            score += 25
+                        }
+                    }
+                }
+
+                return connection.execute('update guessings set score = ? where id = ?', [
+                    score,
+                    guess.id
+                ])
+            })
+
+            await Promise.all(batch)
+        }
+        res.redirect('/admin/games')
+    })
+
+    app.get('/games/delete/:id', async (req, res) => {
         await connection.execute('delete from games where id = ? limit 1', [
             req.params.id
         ])
